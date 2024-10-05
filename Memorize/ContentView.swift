5149687908354d725e821f12f89a8cebc9e3b1a3
Tarun Vyas <tarunvyas@maglabs.in>
2024-10-05 15:22:49 +0530

    CardView added, HStack added, VStack Removed

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,12 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundStyle(.orange)
         .padding()
     }
 }
+
+
+struct CardView: View {
+    var isFaceUp = false
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }
+        } else {
+            RoundedRectangle(cornerRadius: 12)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
