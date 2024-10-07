e63c27a7c6ae47567610c6a684d815c423a3326a
Tarun Vyas <tarunvyas@maglabs.in>
2024-10-07 18:51:05 +0530

    New Buttons with Respective Themes Cleaned code up

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,32 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var emojis = ["👻","🎃","😈","🕷️","💀","☠️","👽","🤖","👾","👿","👹"]
+    
+    @State var cardCount = 4
+    
     var body: some View {
-        HStack {
-            CardView(isFaceUp: true)
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            Text("Memorize!").font(.largeTitle)
+            ScrollView {
+                cards
+            }
+            Spacer()
+            buttonsView
+        }
+        .padding()
+    }
+    
+    var cards : some View {
+        LazyVGrid(columns:[GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
         .foregroundStyle(.orange)
+    }
+    
+    var buttonsView: some View {
+        HStack {
+            halloweenThemeButton
+            Spacer()
+            carThemeButton
+            Spacer()
+            sportsThemeButton
+        }
         .padding()
+    }
+    
+    var halloweenThemeButton: some View {
+        Button(action: {
+            emojis = ["👻","🎃","😈","🕷️","💀","☠️","👽","🤖","👾","👿","👹"].shuffled()
+        }, label: {
+            VStack {
+                Image(systemName: "arrow.2.circlepath.circle")
+                Text("Halloween")
+            }
+            .imageScale(.large)
+        })
+    }
+    
+    var carThemeButton: some View {
+        Button(action: {
+            emojis = ["🚗","🚙","🏎️","🚕","🚓"].shuffled()
+        }, label: {
+            VStack {
+                Image(systemName: "car")
+                Text("Car")
+            }
+            .imageScale(.large)
+        })
+    }
+    
+    var sportsThemeButton: some View {
+        Button(action: {
+            emojis = ["⚽️","🏀","🏈","⚾️","🎾"].shuffled()
+        }, label: {
+            VStack {
+                Image(systemName: "football")
+                Text("Sports")
+            }
+            .imageScale(.large)
+        })
     }
 }
 
 
 struct CardView: View {
-    var isFaceUp = false
+    let content: String
+    @State var isFaceUp = false
+
     var body: some View {
-        if isFaceUp {
-            ZStack {
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .stroke(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.stroke(lineWidth: 2)
+                Text(content).font(.largeTitle)
             }
-        } else {
-            RoundedRectangle(cornerRadius: 12)
+            .opacity(isFaceUp ? 1 : 0) // Adjusts opacity of emoji, if not used they would till change sizes
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
